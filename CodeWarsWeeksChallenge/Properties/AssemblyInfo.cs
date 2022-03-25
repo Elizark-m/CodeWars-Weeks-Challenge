302a12c2778d6f5e5ef431b3ba723d20c6e2776a
Elizark-m <85119923+Elizark-m@users.noreply.github.com>
2022-03-25 16:13:59 +0400

    Update AssemblyInfo.cs

--- CodeWarsWeeksChallenge/Properties/AssemblyInfo.cs
+++ CodeWarsWeeksChallenge/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("CodeWarsWeeksChalange")]
+[assembly: AssemblyTitle("CodeWarsWeeksChallenge")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CodeWarsWeeksChalange")]
+[assembly: AssemblyProduct("CodeWarsWeeksChallenge")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
