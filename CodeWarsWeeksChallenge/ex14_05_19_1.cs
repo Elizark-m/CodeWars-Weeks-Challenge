f1f073dcb71acb699ab61c406ac8ea5b65d7a17c
Elizark-m <85119923+Elizark-m@users.noreply.github.com>
2022-03-25 16:08:19 +0400

    Update ex14_05_19_1.cs

--- CodeWarsWeeksChallenge/ex14_05_19_1.cs
+++ CodeWarsWeeksChallenge/ex14_05_19_1.cs
@@ -35,16 +35,22 @@ namespace CodeWarsWeeksChallenge
         protected override void Main()
         {   
             Random rnd = new Random();
+            // формирует случайное число
             string str = rnd.Next(100, 10000).ToString();
             Console.WriteLine(str);
-
+            
+            // формирование массива для цифр в числе str
             int[] start = new int[str.Length];
+            // формирование массива для цифр результирующего числа
             int[] res = new int[str.Length];
+            // перевод числа в массив
             for(int i = 0; i < str.Length; i++)
                 Int32.TryParse(""+str[i],out start[i]);
-
+            
+            // сортировка полученного массива
             quicksort(start, 0, start.Length-1);
-
+            
+            // заполнение результирующего массива начальным в обратном порядке
             for (int i = 0; i < str.Length; i++)
                 res[i] = start[str.Length-1 - i];
 
@@ -57,21 +63,30 @@ namespace CodeWarsWeeksChallenge
         int partition<T>(T[] m, int a, int b) where T : IComparable<T>
         {
             int i = a;
-            for (int j = a; j <= b; j++)         // просматриваем с a по b
+            // просматриваем с a по b
+            for (int j = a; j <= b; j++)         
             {
-                if (m[j].CompareTo(m[b]) <= 0)  // если элемент m[j] не превосходит m[b],
+                // если элемент m[j] не превосходит m[b],
+                // меняем местами m[j] и m[a], m[a+1], m[a+2] и так далее...
+                // то есть переносим элементы меньшие m[b] в начало,
+                // а затем и сам m[b] «сверху»
+                // таким образом последний обмен: m[b] и m[i], после чего i++
+                if (m[j].CompareTo(m[b]) <= 0)  
                 {
-                    T t = m[i];                  // меняем местами m[j] и m[a], m[a+1], m[a+2] и так далее...
-                    m[i] = m[j];                 // то есть переносим элементы меньшие m[b] в начало,
-                    m[j] = t;                    // а затем и сам m[b] «сверху»
-                    i++;                         // таким образом последний обмен: m[b] и m[i], после чего i++
+                    T t = m[i];                  
+                    m[i] = m[j];                 
+                    m[j] = t;                    
+                    i++;                         
                 }
             }
-            return i - 1;                        // в индексе i хранится <новая позиция элемента m[b]> + 1
+            // в индексе i хранится <новая позиция элемента m[b]> + 1
+            return i - 1;                        
         }
-
-        void quicksort<T>(T[] m, int a, int b) where T : IComparable<T>// a - начало подмножества, b - конец
-        {                                        // для первого вызова: a = 0, b = <элементов в массиве> - 1
+        
+        // a - начало подмножества, b - конец
+        // для первого вызова: a = 0, b = <элементов в массиве> - 1
+        void quicksort<T>(T[] m, int a, int b) where T : IComparable<T>
+        {                                        
             if (a >= b) return;
             int c = partition(m, a, b);
             quicksort(m, a, c - 1);
